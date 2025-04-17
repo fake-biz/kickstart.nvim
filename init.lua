2cf80e9ddaf92a1dba734bc6e583fb97d55f4c31
Harry Anderson <14777088+hrryndrsn@users.noreply.github.com>
2025-04-17 13:31:55 +1000

    feat: open diagnostics float command

--- init.lua
+++ init.lua
@@ -176,6 +176,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Map a key to show diagnostics under the cursor
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostics under cursor' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
